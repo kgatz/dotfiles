a5e4e5b80b3df0cbcacf63aebd0a40b0f02bdc3e
Kai Gatzweiler <kgatzweiler@opentext.com>
2021-12-31 00:07:43 +0100

    Adding new files

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,10 @@
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+function ....  ; cd ../../.. ; end
+function ..... ; cd ../../../.. ; end
+
+
+# Utilities
+function g        ; git $argv ; end
+function grep     ; command grep --color=auto $argv ; end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
+source ~/.config/fish/aliases.fish
+
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end

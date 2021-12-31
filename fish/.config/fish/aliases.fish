2f95e23147a2efd6889d048fdf102aa80a5f5b43
Kai Gatzweiler <kgatzweiler@opentext.com>
2021-12-31 01:34:40 +0100

    Switching to nvim

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -8,3 +8,6 @@ function ..... ; cd ../../../.. ; end
 # Utilities
 function g        ; git $argv ; end
 function grep     ; command grep --color=auto $argv ; end
+
+alias vi="nvim"
+alias vim="nvim"

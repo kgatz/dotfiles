88f66e07417822f498294032daccd7a4b1e276e2
Kai Gatzweiler <kgatzweiler@opentext.com>
2022-01-02 00:33:13 +0100

    Fix Git for WSL

--- run_03_install_brew_bundles.sh
+++ run_03_install_brew_bundles.sh
@@ -14,9 +14,6 @@ if [ -d ~/.config/fish/completions ]; then
     if command -v chezmoi &> /dev/null; then
         chezmoi completion fish --output ~/.config/fish/completions/chezmoi.fish
     fi
-    if command -v flux &> /dev/null; then
-        flux completion fish > ~/.config/fish/completions/flux.fish
-    fi
     if command -v helm &> /dev/null; then
         helm completion fish > ~/.config/fish/completions/helm.fish
     fi

236c5f34e505aa459227ca8b367a23a64412b801
Kai Gatzweiler <kgatzweiler@opentext.com>
2022-01-02 13:39:27 +0100

    Adding dtags function

--- dot_config/fish/functions/dtags.fish
+++ dot_config/fish/functions/dtags.fish
@@ -1,4 +1,4 @@
 function dtags
-  curl "https://registry.hub.docker.com/v1/repositories/ubuntu/tags" --silent | jq '.[].name' | fzf
+  curl "https://registry.hub.docker.com/v1/repositories/$argv/tags" --silent | jq '.[].name' | fzf
   end
   

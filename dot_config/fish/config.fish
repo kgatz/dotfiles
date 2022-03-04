a5ad4237a11643095591e7d595f947e769ba6b3b
kgatz <kai.gatzweiler@gmail.com>
2022-03-04 06:42:45 +0100

    Add Terraform custom.tfvars

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -11,7 +11,9 @@ alias kns="kubie ns"
 alias k="kubectl"
 
 alias tf="terraform"
-
+set -xg TF_CLI_ARGS_plan "-var-file custom.tfvars"   
+set -xg TF_CLI_ARGS_destroy "-var-file custom.tfvars"
+set -xg TF_CLI_ARGS_apply "-var-file custom.tfvars"  
 
 alias drun='docker run --rm -it -v $PWD:/work -w /work'
 
